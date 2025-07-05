7079c24082fb0d2f648ffbbb520dfbe127833239
Matthew Gregg <mcg@braintube.com>
2025-07-05 10:45:20 -0400

    Remove debug output

--- weather.py
+++ weather.py
@@ -25,6 +25,10 @@ def fetch_xml_feed():
     # Parse the XML content
     soup = BeautifulSoup(xml_content, 'xml')
     no_storms = soup.find(string=re.compile(r'Tropical cyclone formation is not expected during the next 7 days', re.IGNORECASE)) is not None
+    pattern = re.compile(r'.*(Tropical Storm|Hurricane).*Graphics.*', re.IGNORECASE)
+    titles = soup.find_all('title', string=pattern)
+    if len(titles) > 0:
+        no_storms = False
 
     return no_storms, soup
 
@@ -48,8 +52,9 @@ def find_cyclones_in_feed(soup, map_name):
             if match:
                 storm_name = match.group(2).strip()
                 storm_type = match.group(1).strip()
-                if storm_type == 'Hurricane':
+                if storm_type == 'Hurricane' or storm_type == 'Tropical Storm':
                     cyclones.append({'storm_name': storm_name, 'image_url': img_tag['src']})
+                    print(f"Found cyclone: {storm_name} with image URL: {img_tag['src'] if img_tag else 'N/A'}")
     return cyclones
 
 def generate_cyclone_images(soup, map_name, image_file_path):
@@ -64,6 +69,9 @@ def generate_cyclone_images(soup, map_name, image_file_path):
         gif_file_name = f"{image_file_path}/{cyclone['storm_name']}_{map_name}.gif"
 
         # Handle the response
+        if response.from_cache:
+            print(f"{cyclone['storm_name']} image from cache")
+
         if not response.from_cache:
             print(f"{cyclone['storm_name']} image not from cache")
             with open(image_file_name, 'wb') as image_file:
@@ -95,6 +103,7 @@ def generate_cyclone_images(soup, map_name, image_file_path):
                 # Save the updated GIF with the new frame
                 frames[0].save(gif_file_name, save_all=True, append_images=frames[1:], loop=0, duration=500)
                 all_images.append({'png': image_file_name, 'gif': gif_file_name, 'name': cyclone['storm_name']})
+                print(f"Updated GIF for {all_images} with new frame.")
 
     return url, all_images
 
@@ -211,7 +220,7 @@ def upload_to_slack_and_discord(images, image_file_name, gif_file_name, slack_to
             channel="C2BRCNET1",
             # test channel
             #channel="C07KTS31M1T",
-            initial_comment="Atlantic Tropical Weather Update - Last Modified: " + last_modified + " ETag: " + etag,
+            initial_comment="Atlantic Tropical Weather Update", 
         )
 
     except SlackApiError as e:
@@ -253,6 +262,7 @@ if __name__ == "__main__":
     if no_storms is False:
         url, image_file_name, gif_file_name, image_response = fetch_and_process_image(args.image_file_path)
         url, images = generate_cyclone_images(soup, '5day_cone_with_line_and_wind',args.image_file_path)
+        print(f"Found {len(images)} cyclones in the feed.")
         generate_rss_feed(url, args.rss_file_path, image_response)
         upload_to_slack_and_discord(images, image_file_name, gif_file_name, args.slack_token, args.discord_webhook_url, image_response)
     else:
